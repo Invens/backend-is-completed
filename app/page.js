0c15eea1de08338ab4db0c3f773846527a6eaac3
Abhishek kumar <abhisec.tech@gmail.com>
2024-05-12 03:48:21 -0700

    backend is completed with login

--- app/page.js
+++ app/page.js
@@ -1,38 +1,18 @@
 "use client";
-import Dashboard from '@/components/Dashboard'; // Change to Page
-import Login from '@/components/Login';
-import PrivateRoute from '@/components/PrivateRoute';
-import ProjectImageUpload from './ProjectImageUpload';
+import Page from './login/page';
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import FileUploadForm from './CategoryUpload';
-import LeftVerticalMenu from '@/components/LeftVerticalMenu';
 
-const Page = () => {
+
+const mage = () => {
   // Simulate authentication and OTP verification status
   const isAuthenticated = true; // Replace with actual authentication status
   const isVerified = true; // Replace with actual OTP verification status
 
   return (
     <>
-      <FileUploadForm/>
-      <ProjectImageUpload/>
-      {/* <LeftVerticalMenu/> */}
-     {/* <Dashboard/> */}
-    {/* <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <PrivateRoute
-          path="/dashboard"
-          component={Dashboard}
-          isAuthenticated={isAuthenticated}
-          isVerified={isVerified}
-        />
-        <Redirect from="/" to="/dashboard" />
-      </Switch>
-    </Router> */}
+      <Page/>
     </>
   );
 };
 
-export default Page;
+export default mage;
